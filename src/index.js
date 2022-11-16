e2f694fc4a42b2125b031f15980789334651cdee
eduardovarela27 <eduardoemanuelvarela@gmail.com>
2022-11-16 13:28:19 -0600

    Definition Project Stucture

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Main from "./components/Main";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <h1>Hello World!</h1>
+    <Main/>
   </>
 );
 
